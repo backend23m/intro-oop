15254679ba4f2af72cd4104a9f5efc385fc586aa
djumanov <diyorbekjumanov1120@gmail.com>
2023-11-17 10:05:50 +0500

    total

--- methods.py
+++ methods.py
@@ -1,4 +1,6 @@
 class Mobile:
+    total_price = 0
+    inctances = []
 
     def __init__(self, name: str, ram: int, memory: int, color: str, price: float, brend: str, quantity: int):
         self.name     = name
@@ -9,27 +11,17 @@ class Mobile:
         self.brend    = brend
         self.quantity = quantity
 
+        Mobile.total_price += self.total()
+        Mobile.inctances.append(self)
+
     def info(self) -> str:
         return f"{self.name} costs {self.price}"
 
-m1 = Mobile(
-    name='Samsung s21',
-    ram=6,
-    memory=128,
-    color='black',
-    price=800.00,
-    brend='Samsung',
-    quantity=12
-)
-m2 = Mobile(
-    name='Iphone 14',
-    ram=6,
-    memory=256,
-    color='gray',
-    price=900.00,
-    brend='Iphone',
-    quantity=7
-)
+    def total(self) -> float:
+        return self.price * self.quantity
 
-print(m1.info())
-print(m2.info())
+m1 = Mobile(name='Samsung s21', ram=6, memory=128, color='black', price=800.00, brend='Samsung', quantity=12)
+m2 = Mobile(name='Iphone 14', ram=6, memory=256, color='gray', price=900.00, brend='Iphone', quantity=7)
+
+print(Mobile.total_price)
+print(Mobile.inctances)
